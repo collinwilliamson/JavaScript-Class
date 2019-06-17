456c5d75bc4d86635f1f03d9bc9211e11cb0f1a4
Collin Williamson <williamson@cvpmbp10401.networksplus.local>
2019-06-17 09:54:35 -0500

    Homework Update

--- Class_2/helper.js
+++ Class_2/helper.js
@@ -6,3 +6,20 @@ function echo(arg) {
   p.textContent = arg;
   printArea.appendChild(p);
 } 
+
+
+
+let sign = prompt("What's your birthday?");
+
+let today = new Date();
+
+
+let date2 = new Date(today);
+let date1 = new Date(sign);
+let timeDiff = Math.abs(date2 - date1);
+let dayDifference = Math.ceil(timeDiff / (1000 * 3600 * 24) - 1);
+
+if (sign != null) {
+  alert(
+  "Hello! You are " + dayDifference + " days old.");
+}
