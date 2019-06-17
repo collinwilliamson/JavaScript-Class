dd6b99d98d135818fd0ce7ebb6bc000e8a3def7d
Collin Williamson <williamson@cvpmbp10401.networksplus.local>
2019-06-17 13:18:54 -0500

    homework

--- Class_1/script.js
+++ Class_1/script.js
@@ -2,4 +2,4 @@ var date1 = new Date('July 4, 1776 03:24:00');
 // Sun Dec 17 1995 03:24:00 GMT...
 
 
-document.body.innerHTML = "<h1>"+ date1+ "</h1>";
+document.body.innerHTML = "<h1>"+ date1 + "</h1>";
