9a030353af9c8dfc0662fde336f5e5421555ba94
Collin Williamson <williamson@cvpmbp10401.networksplus.local>
2019-06-24 17:44:22 -0500

    Updates class4

---
+++ Class_4/homework2.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+
+function echo(arg) {
+    const printArea = document.getElementById("main");
+    const p = document.createElement("p");
+    p.textContent = arg;
+    printArea.appendChild(p);
+}
+
+let num1 = prompt("Enter your first number.");
+let num2 = prompt("Enter your second number.");
+
+function productSum(str1, str2) {
+    for (var i=0; i < num2; i++) {
+    }
+    echo();
+}
+
+
+let product = (num1 * num2);
