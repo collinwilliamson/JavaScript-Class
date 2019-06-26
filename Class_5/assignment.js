e143efdf3094f47788c9d3f761ba24da8cf0c483
Collin Williamson <williamson@cvpmbp10401.networksplus.local>
2019-06-26 15:30:40 -0500

    Update to homework

--- Class_5/assignment.js
+++ Class_5/assignment.js
@@ -1,35 +1,18 @@
-/*1. (13 pts) Create an object constructor function that represents a movie. 
-The object should have ratings property that is an array of the rating numbers, and avgRating that should 
-be average of the items in the array. The object should have a method addRating that adds rating to the 
-array and also updates the averageRating.
+/* `1. (11 pts) Define a function charCount that takes a string as an argument and returns an object containing character count of letters.
 
 ```javascript
-// Assuming object is already defined above, one should be able to do the following:
-const movie = new Movie("The Lego Movie"); // Create the object
-console.log(movie.title); // Output the movie title;
-console.log(movie.avgRating); // Output the current average rating. Should be 0.
-movie.addRating(10); // This movie is awesome, let's give it 10 stars.
-console.log(movie.avgRating); // Output the current average rating.
-movie.addRating(6); // Not everyone liked it :(
-console.log(movie.avgRating); // Should be 8.
-console.log(movie.ratings); // Should output Array [10, 6]
+const result = charCount("Pack my box with five dozen liquor jugs.");
+const aCount = result.a;
+const spaceCount = result[' '];
+console.log(aCount); // Should output 1
+console.log(spaceCount); // Should output 7
+```
+
+P.S. Create the separate .js files for each assignment and homework. Create two branches, one for assignments and one for homework. Push your changes to the corresponding Github branches. Create pull requests to your master.
 */
 
-function Movie (title) {
-  this.title = title;
-  this.ratings = [];
-  this.addRating = function(rating) {
-    this.ratings.push(rating);
-    let total =0;
-    for (let i = 0; i < this.ratings.length; i++) {
-        total += this.ratings[i];
-    }
-    this.avgRating = total / this.ratings.length;
-  };
-  this.avgRating = 0;
-  
-    
+function charCount(result) {
+  this.result = result;
+  this.aCount = result.match("a").length;
+  this.spaceCount = (result.split(" ").length - 1)
 }
-
-const myMovie = new Movie("Lego movie");
-
